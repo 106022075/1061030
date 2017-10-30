82b67a709d05a548e25eb05278b14c59e18c572e
unknown <joanna225940081@yahoo.com.tw>
2017-10-30 14:25:58 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int a;
+        a=in.nextInt();
+        if(a%2==1)
+        System.out.println("奇");
+        int b;
+        b=in.nextInt();
+        if(a%2==0)
+        System.out.println("偶");
+    }
+}
